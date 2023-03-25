202e55ed4ae2c5aa6ebca3f51de00e309938695d
UberFlame <103065297+UberFlame@users.noreply.github.com>
2023-03-25 08:48:05 +0300

    push

--- СУБД_Кадры_Белоус/СУБД_Кадры_Белоус/Program.cs
+++ СУБД_Кадры_Белоус/СУБД_Кадры_Белоус/Program.cs
@@ -16,7 +16,7 @@ namespace СУБД_Кадры_Белоус
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new FormMain());
+            Application.Run(new FormMain()); 
         }
     }
 }
